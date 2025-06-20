71e25e6a54725d2dd1916c166f86a55b1f9182be
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 13:07:34 +0000

    [automatic commit] 2025-06-20 13:07:34 UTC 103.232.160.137

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -23,7 +23,7 @@ createApp({
 
     function scrollToAbout() {
       const about = document.getElementById('about-section');
-      about.scrollIntoView();
+      about.scrollIntoView(ease);
     }
 
     onMounted(() => {
