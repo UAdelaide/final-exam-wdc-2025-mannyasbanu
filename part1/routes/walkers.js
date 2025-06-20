f56251f0bee75acc0d159f786bd4f9db09a14e05
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 09:27:30 +0000

    [automatic commit] 2025-06-20 09:27:30 UTC 103.232.160.137

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -20,7 +20,7 @@ router.get('/summary', async (req, res, next) => {
         INNER JOIN Users ON WalkRatings.walker_id = Users.user_id
         WHERE Users.username = '${walker.username}'
       `);
-      walker.append
+      walker[]
     });
   } catch (error) {
     // Error handling
