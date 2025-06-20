272ca104967b73ef9453c5219b35175b0897eb28
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 08:40:07 +0000

    [automatic commit] 2025-06-20 08:40:07 UTC 103.232.160.137

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -6,7 +6,9 @@ const db = require('../db');
 router.get('/', async (req, res, next) => {
   try {
     const [rows] = await db.query('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
-    res.send(JSON.stringify(rows));
+    rows.forEach(row => {
+      res.send(JSON.stringify(rows));
+    });
   } catch (error) {
 
   }
