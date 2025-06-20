125e5ea6cd2edc74a232020e69aaac6af9167423
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 11:26:18 +0000

    [automatic commit] 2025-06-20 11:26:18 UTC 103.232.160.137

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -1,13 +1,15 @@
 const vueinst = new Vue({
   el: '#app',
   data: {
-    dogimg: ''
+    dogImgUrl: ''
   },
   mounted(){
     // Fetch dog image of the day
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-      if(xhttp.readyState == 4 && xhttp.status == 200)
+      if(xhttp.readyState == 4 && xhttp.status == 200){
+        dogimg =
+      }
     }
 
   },
