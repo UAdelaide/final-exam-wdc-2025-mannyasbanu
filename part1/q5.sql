0737fc8016ca0a72ea6ee3cbc1e7c9bac96473b4
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 06:41:41 +0000

    [automatic commit] 2025-06-20 06:41:41 UTC 103.232.160.137

--- part1/q5.sql
+++ part1/q5.sql
@@ -1,7 +1,8 @@
-
+/*5 USERS*/
 INSERT INTO Users (username, email, password_hash, role)
 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('manny', 'manny@example.com', 'hashed666', 'walker'),
 ('aish', 'aish@example.com', 'hashed444', 'owner');
+/*5 USERS*/
