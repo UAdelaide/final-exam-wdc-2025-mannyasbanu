3257c1c7f39af32dce074a481e07ef069aae63e9
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 07:41:22 +0000

    [automatic commit] 2025-06-20 07:41:22 UTC 103.232.160.137

--- part1/app.js
+++ part1/app.js
@@ -36,20 +36,28 @@ let db;
     // Create tables
     await db.execute(`
       CREATE TABLE Users (
-      user_id INT AUTO_INCREMENT PRIMARY KEY,
-      username VARCHAR(50) UNIQUE NOT NULL,
-      email VARCHAR(100) UNIQUE NOT NULL,
-      password_hash VARCHAR(255) NOT NULL,
-      role ENUM('owner', 'walker') NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
     `);
     await db.execute(`
-    CREATE TABLE Dogs (
-      dog_id INT AUTO_INCREMENT PRIMARY KEY,
-      owner_id INT NOT NULL,
-      name VARCHAR(50) NOT NULL,
-      size ENUM('small', 'medium', 'large') NOT NULL,
-      FOREIGN KEY (owner_id) REFERENCES Users(user_id))
+      CREATE TABLE Dogs (
+        dog_id INT AUTO_INCREMENT PRIMARY KEY,
+        owner_id INT NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        size ENUM('small', 'medium', 'large') NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id))
+    `);
+    await db.execute(`
+      CREATE TABLE Dogs (
+        dog_id INT AUTO_INCREMENT PRIMARY KEY,
+        owner_id INT NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        size ENUM('small', 'medium', 'large') NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id))
     `);
 
     // Insert data if table is empty
