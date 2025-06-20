26a38b61b4469ffd3713acb92af5b3e0e79501f1
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 06:49:03 +0000

    [automatic commit] 2025-06-20 06:49:03 UTC 103.232.160.137

--- part1/q5.sql
+++ part1/q5.sql
@@ -8,8 +8,8 @@ VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 /*5 DOGS*/
 INSERT INTO Dogs (name, size, owner_id)
 VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+('Jobbot', 'medium', (SELECT user_id FROM Users WHERE username = 'carol123')),
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'carol123')),
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'aish')),
-('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'carol123')),
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'aish'))
 /*5 WALK REQUESTS*/
