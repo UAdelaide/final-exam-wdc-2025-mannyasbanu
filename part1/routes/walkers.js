51433810aeae59c472b28bc707048a26fc8cb64d
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 09:36:35 +0000

    [automatic commit] 2025-06-20 09:36:35 UTC 103.232.160.137

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -7,21 +7,7 @@ const db = require('../db');
 router.get('/summary', async (req, res, next) => {
   try {
     // Query walkers
-    const [walkers] = await db.query(`
-      SELECT username AS walker_username
-      FROM Users
-      WHERE role = 'walker';
-    `);
-    // Query total ratings
-    for (const walker of walkers) {
-      const [ratings] = await db.query(`
-        SELECT COUNT(WalkRatings.rating_id)
-        FROM WalkRatings
-        INNER JOIN Users ON WalkRatings.walker_id = Users.user_id
-        WHERE Users.username = '?'
-      `, [walker.username]);
-      walker.total_ratings = ratings[0];
-    };
+    
   } catch (error) {
     // Error handling
     console.error(error);
