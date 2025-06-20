7ab8e96d4505a85eceee70c21583e3556f359a38
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 09:19:38 +0000

    [automatic commit] 2025-06-20 09:19:38 UTC 103.232.160.137

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -8,7 +8,7 @@ router.get('/summary', async (req, res, next) => {
   try {
     // Query rows
     const [rows] = await db.query(`
-      SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id
+      SELECT username AS walker_username
     `);
   } catch (error) {
     // Error handling
