b91ea707acf40aec5b2dfc3e46df6d5f03c57e45
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 07:42:02 +0000

    [automatic commit] 2025-06-20 07:42:02 UTC 103.232.160.137

--- part1/app.js
+++ part1/app.js
@@ -53,23 +53,26 @@ let db;
     `);
     await db.execute(`
       CREATE TABLE WalkRequests (
-    request_id INT AUTO_INCREMENT PRIMARY KEY,
-    dog_id INT NOT NULL,
-    requested_time DATETIME NOT NULL,
-    duration_minutes INT NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
-);
+        request_id INT AUTO_INCREMENT PRIMARY KEY,
+        dog_id INT NOT NULL,
+        requested_time DATETIME NOT NULL,
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id))
     `);
     await db.execute(`
-      CREATE TABLE Dogs (
-        dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        owner_id INT NOT NULL,
-        name VARCHAR(50) NOT NULL,
-        size ENUM('small', 'medium', 'large') NOT NULL,
-        FOREIGN KEY (owner_id) REFERENCES Users(user_id))
+      CREATE TABLE WalkApplications (
+    application_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT NOT NULL,
+    walker_id INT NOT NULL,
+    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+    CONSTRAINT unique_application UNIQUE (request_id, walker_id)
+)
     `);
     await db.execute(`
       CREATE TABLE Dogs (
