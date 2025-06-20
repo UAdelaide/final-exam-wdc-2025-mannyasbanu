2a682971977de1b1f08ff7c44b515424b95b501d
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 12:15:48 +0000

    [automatic commit] 2025-06-20 12:15:48 UTC 103.232.160.137

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -11,8 +11,8 @@ createApp({
         const x = Math.random() * 1000;
         const y = Math.random() * 1000;
         button.style.position = 'fixed';
-        button.style.left = x;
-        button.style.top = y + ;
+        button.style.left = x + 'px';
+        button.style.top = y + 'px';
       });
     }
 
