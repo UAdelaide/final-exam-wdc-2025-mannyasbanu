ec3c912a0f58d93e2f3ae9ff9420c392dd79f0c7
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 07:43:42 +0000

    [automatic commit] 2025-06-20 07:43:42 UTC 103.232.160.137

--- part1/app.js
+++ part1/app.js
@@ -89,9 +89,14 @@ let db;
     `);
 
     // Insert data
-    await db.execute('
-
-      ');
+    await db.execute(`
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+      ('manny', 'manny@example.com', 'hashed666', 'walker'),
+      ('guts', 'guts@example.com', 'hashed444', 'owner')
+      `);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
