b686321e3bdbc1c0d19bccece87d3cdc938d1309
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 07:45:02 +0000

    [automatic commit] 2025-06-20 07:45:02 UTC 103.232.160.137

--- part1/app.js
+++ part1/app.js
@@ -98,6 +98,22 @@ let db;
       ('manny', 'manny@example.com', 'hashed666', 'walker'),
       ('guts', 'guts@example.com', 'hashed444', 'owner')
       `); //Dogs
+      await db.execute(`
+      INSERT INTO Dogs (name, size, owner_id)
+VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+('Bogbog', 'large', (SELECT user_id FROM Users WHERE username = 'carol123')),
+('Minh', 'small', (SELECT user_id FROM Users WHERE username = 'guts')),
+('Griffith', 'large', (SELECT user_id FROM Users WHERE username = 'guts'))
+      `); // WalkRequests
+      await db.execute(`
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+      ('manny', 'manny@example.com', 'hashed666', 'walker'),
+      ('guts', 'guts@example.com', 'hashed444', 'owner')
+      `);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
