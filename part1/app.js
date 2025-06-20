923083ff0467ae8d5b36695688b3d17796e5482a
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 07:27:53 +0000

    [automatic commit] 2025-06-20 07:27:53 UTC 103.232.160.137

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,7 @@ let db;
       database: 'DogWalkService'
     });
 
-    // Create a table if it doesn't exist
+    // Insert
     await db.execute(`
       CREATE TABLE IF NOT EXISTS books (
         id INT AUTO_INCREMENT PRIMARY KEY,
