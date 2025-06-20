56fc7a30dfec511fcce1ae90fc64044bfc6580f7
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 11:51:42 +0000

    [automatic commit] 2025-06-20 11:51:42 UTC 103.232.160.137

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -1,22 +1,23 @@
-const vueinst = new Vue({
-  el: '#app',
-  data: {
-    dogImgUrl: ''
-  },
-  mounted(){
-    // Fetch dog image of the day
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
-      if(xhttp.readyState == 4 && xhttp.status == 200){
-         const res = JSON.parse(xhttp.responseText);
-         this.dogImgUrl = res.message;
-      } else {
-        console.log('failed to fetch dogImg');
-      }
-    };
-    xhttp.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
-    xhttp.send();
-  },
-  methods: {
+const { createApp, ref, onMounted } = Vue;
+
+createApp({
+  setup() {
+    const dogImgUrl = ref('');
+
+    onMounted(() => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+          const res = JSON.parse(xhttp.responseText);
+          dogImgUrl.value = res.message;
+        } else if (xhttp.readyState === 4) {
+          console.log('failed to fetch dogImg');
+        }
+      };
+      xhttp.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+      xhttp.send();
+    });
+
+    return { dogImgUrl };
   }
-});
+}).mount('#app');
