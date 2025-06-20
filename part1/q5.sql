977c573b09ec35d99b61e94891869541be7e3e19
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 06:55:24 +0000

    [automatic commit] 2025-06-20 06:55:24 UTC 103.232.160.137

--- part1/q5.sql
+++ part1/q5.sql
@@ -16,3 +16,6 @@ VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES ((SELECT dog_id FROM Dogs WHERE name = 'Max'), 2025-06-10 08:00:00, 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), 2025-06-10 08:00:00, 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), 2025-06-10 08:00:00, 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), 2025-06-10 08:00:00, 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), 2025-06-10 08:00:00, 30, 'Parklands', 'open');
