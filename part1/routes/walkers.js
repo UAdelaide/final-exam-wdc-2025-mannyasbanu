92aa38fc911f2a452cb829ebd9bd4e3defd5e21d
mannyasbanu <asbanu.manny@gmail.com>
2025-06-20 09:06:54 +0000

    [automatic commit] 2025-06-20 09:06:54 UTC 103.232.160.137

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // Import pool
 const db = require('../db');
 
-// 
+// GET request for /api/walkers/summary
+routers.get('/summary', async ())
 
 module.exports = router;
