edcb2582fecf9d15e38e911a7cbc47a4b042a72d
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 03:31:34 +0000

    [automatic commit] 2025-06-21 03:31:34 UTC 103.232.160.137

--- part2/public/javascripts/index.js
+++ part2/public/javascripts/index.js
@@ -1,3 +1,35 @@
+// LOGIN FUNCTION
+async function login(username, password){
+    console.log("Sending login request");
+    try{
+        // Post login request
+        const res = await fetch('/api/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+            credentials: 'include'
+        });
+        // Parse response
+        const data = await res.json();
+        // Validate response
+        if(!response.ok){
+            throw new Error(data.error || 'Login failed');
+        }
+        // Redirect according to role
+        if(data.role === 'owner'){
+            window.location.href = '/owner';
+        }else if(data.role === 'walker'){
+            window.location.href = '/walker';
+        }else {
+            throw new Error('Unknown user role');
+        }
+        alert('Welcome ', data.username);
+    } catch (error){
+        console.error('Login error: ', error);
+        alert(error.message || 'Login failed');
+    }
+}
+
 // LOGIN EVENT LISTENER
 document.getElementById('login-form').addEventListener('submit', function(event) {
     console.log("Submitting login form");
