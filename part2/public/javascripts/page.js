81919fb5b9aa76dd65ee551ab1f99bb96109c8b5
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 02:53:22 +0000

    [automatic commit] 2025-06-21 02:53:22 UTC 103.232.160.137

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -188,7 +188,7 @@ function login(username, password){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if(this.)
+            if(this.response.succ)
 
             alert("Welcome "+this.responseText);
         } else if (this.readyState == 4 && this.status >= 400) {
