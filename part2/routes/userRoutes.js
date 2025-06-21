85fb8bc4030088cb794692c13b8f9357becb887a
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 01:22:44 +0000

    [automatic commit] 2025-06-21 01:22:44 UTC 103.232.160.137

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // Store session info
+    // Store session information
+    
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
