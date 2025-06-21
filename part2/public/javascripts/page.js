35f956e94293b90355177f9f76aa8d38fca2aed7
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 03:21:51 +0000

    [automatic commit] 2025-06-21 03:21:51 UTC 103.232.160.137

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -179,7 +179,8 @@ async function login(username, password){
         const res = await fetch('/api/users/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify
+            body: JSON.stringify({ username, password }),
+            credentials
         })
     }
 
