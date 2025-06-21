4c6d1bf5b4c713b4b1b4e741d46a1b8b4ac67dc3
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 03:27:12 +0000

    [automatic commit] 2025-06-21 03:27:12 UTC 103.232.160.137

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -203,28 +203,6 @@ async function login(username, password){
         console.error('Login error: ', error);
         alert(error.message || 'Login failed');
     }
-
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
-
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if(this.response.success){
-
-            }
-
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "api/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
 }
 
 function logout(){
