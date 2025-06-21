c8740f8415e70e387b88945b39d974f7b045584c
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:43:43 +0000

    [automatic commit] 2025-06-21 04:43:44 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -37,20 +37,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// Authentication middleware
-function loginCheck(req, res, next){
-  if(req.session && req.session.user) return next();
-  res.redirect('/');
-}
-function roleCheck(role){
-  return function(req, res, next){
-    if(!req.session.user){
-      return res.status(401).redirect('/');
-    }
-    if(req.session.user.role === role) return next();
-    res.status(403).send('Access denied');
-  };
-}
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
---
+++ part2/auth.js
@@ -0,0 +1,16 @@
+// Authentication middleware
+function loginCheck(req, res, next){
+  if(req.session && req.session.user) return next();
+  res.redirect('/');
+}
+function roleCheck(role){
+  return function(req, res, next){
+    if(!req.session.user){
+      return res.status(401).redirect('/');
+    }
+    if(req.session.user.role === role) return next();
+    res.status(403).send('Access denied');
+  };
+}
+
+module.exports
