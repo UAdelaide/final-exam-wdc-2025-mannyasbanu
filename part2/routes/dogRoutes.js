2160a07d20d73735b28566548a10c0c21933a8b7
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:38:57 +0000

    [automatic commit] 2025-06-21 04:38:57 UTC 103.232.160.137

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,12 +5,12 @@ const db = require('../models/db');
 router.get('/', loginCheck, async(req, res) => {
   try {
     const [dogs] = await db.query(`
-
+      SELECT 
     `);
     res.json(dogs);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
-})
+});
 
 module.exports = router;
