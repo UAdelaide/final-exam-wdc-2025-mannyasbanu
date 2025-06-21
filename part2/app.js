4ada3c4266602a6e9413a65360920558df90047c
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 02:38:12 +0000

    [automatic commit] 2025-06-21 02:38:12 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -18,6 +18,7 @@ app.use(session({
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
