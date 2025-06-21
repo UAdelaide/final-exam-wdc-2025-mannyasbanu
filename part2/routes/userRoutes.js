28951c291ed2dc8293abd03655943f84f68ebb3c
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 02:14:39 +0000

    [automatic commit] 2025-06-21 02:14:39 UTC 103.232.160.137

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -57,6 +57,7 @@ router.post('/login', async (req, res) => {
       role: user.role
     };
 
+/*
     // Redirect according to role
     if(user.role == 'owner'){
       res.redirect('/owner');
@@ -65,7 +66,7 @@ router.post('/login', async (req, res) => {
     } else {
       res.status(403).send('user role is unknown');
     }
-
+*/
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
