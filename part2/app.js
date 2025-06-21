737e015d9a62edb6987e80487b53a5bf72292007
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:45:04 +0000

    [automatic commit] 2025-06-21 04:45:04 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Auth middleware
-{ loginCheck,  }
+{ loginCheck, roleCheck } = require()
 
 // Default root handler
 app.get('/', (req, res) => {
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dogRoutes = require('./routes/dogRoutes');
-const { loginCheck } = require('./auth');
+const { loginCheck, roleCheck } = require('./auth');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/dogs', dogRoutes);
