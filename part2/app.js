3e0ddadabb5656beb3188facd056f608f6b4029c
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 03:59:00 +0000

    [automatic commit] 2025-06-21 03:59:00 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -29,7 +29,6 @@ app.get('/', (req, res) => {
     } else if(req.session.user.role === 'walker'){
       return res.redirect('/walker');
     } else {
-      
     }
   } // Otherwise redirect to login page
   res.sendFile(path.join(__dirname, '/public/index.html'));
