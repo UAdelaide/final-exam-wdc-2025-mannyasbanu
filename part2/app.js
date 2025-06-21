b72060a2d3baa606652a36857f705be505493554
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:36:17 +0000

    [automatic commit] 2025-06-21 04:36:17 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -55,7 +55,9 @@ function roleCheck(role){
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
 app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/users', userRoutes);
 
 // Protected routes
