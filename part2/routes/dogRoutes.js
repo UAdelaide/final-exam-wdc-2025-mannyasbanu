c1d7810c994b5d1d5300770a172dcdcdd7dcb380
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:37:17 +0000

    [automatic commit] 2025-06-21 04:37:17 UTC 103.232.160.137

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/', )
+router.get('/', loginCheck)
 
 module.exports = router;
