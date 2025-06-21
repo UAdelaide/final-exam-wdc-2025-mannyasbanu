f94bde95c36eac36e95d5f9072de1a2c7ef5f11d
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 01:50:32 +0000

    [automatic commit] 2025-06-21 01:50:32 UTC 103.232.160.137

--- part2/app.js
+++ part2/app.js
@@ -20,6 +20,7 @@ app.use(session({
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+// S
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
