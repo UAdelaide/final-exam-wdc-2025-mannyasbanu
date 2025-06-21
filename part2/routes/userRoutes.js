b7a9d926fb99a635f3c9b31b60dc45d0c3859413
mannyasbanu <asbanu.manny@gmail.com>
2025-06-21 04:27:49 +0000

    [automatic commit] 2025-06-21 04:27:49 UTC 103.232.160.137

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,8 @@ router.post('/login', async (req, res) => {
     }
     const user = rows[0];
 
-    // S
+    // Store cookie
+    con
 
     // Store session information
     req.session.user = {
